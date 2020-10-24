9d35fa90b5662c4be62f4a21d1ced9e5db87f767
Michael S Lee <msblnewsky@gmail.com>
2020-10-24 18:42:06 -0400

    added StringCompression algo

---
+++ src/StringCompression.java
@@ -0,0 +1,23 @@
+public class StringCompression {
+    public static char[] compress(char[] chars) {
+        int anchor = 0, write = 0;
+        for (int read = 0; read < chars.length; read++) {
+            if (read + 1 == chars.length || chars[read + 1] != chars[read]) {
+                chars[write++] = chars[anchor];
+                if (read > anchor) {
+                    for (char c : ("" + (read - anchor + 1)).toCharArray()) {
+                        chars[write++] = c;
+                    }
+                }
+                anchor = read + 1;
+            }
+        }
+        return chars;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+        System.out.println(chars);
+        System.out.println(compress(chars));
+    }
+}
