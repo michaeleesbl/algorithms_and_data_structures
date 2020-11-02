54a4e50bb2da474e816bdb6f62c27a9b675a0e38
Michael S Lee <msblnewsky@gmail.com>
2020-11-01 19:06:40 -0500

    ReverseLinkedList recursion java

---
+++ src/ReverseLinkedList.java
@@ -0,0 +1,36 @@
+public class ReverseLinkedList {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ReverseLinkedList sol = new ReverseLinkedList();
+        ListNode ans = sol.reverseList(head);
+        while (ans != null) {
+            System.out.println(ans.val);
+            ans = ans.next;
+        }
+    }
+}
