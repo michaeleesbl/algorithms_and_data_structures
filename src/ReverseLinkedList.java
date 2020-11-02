d2f9e743b8955ced790a00703558d70ed0220a43
Michael S Lee <msblnewsky@gmail.com>
2020-11-01 19:09:32 -0500

    refactored slightly

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,20 +1,4 @@
 public class ReverseLinkedList {
-    static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
 
     public ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) return head;
